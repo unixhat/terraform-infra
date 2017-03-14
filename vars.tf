c2692f5a715ca49803374d35c4931637f5a7cfff
heriyanto <heriyanto@Changes-MacBook-Pro.local>
2017-03-14 14:28:35 +0700

    add ssh key, do provisioning instance with script.sh, add output function

--- vars.tf
+++ vars.tf
@@ -7,6 +7,16 @@ variable "AMIS" {
   type = "map"
   default = {
     ap-southeast-1 = "ami-03a31060"
-    ap-southeast-1 = "ami-2de5e74e"
+    ap-southeast-2 = "ami-2de5e74e"
   }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
 }
