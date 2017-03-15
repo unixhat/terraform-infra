510acd03dbcc6d2d52243605a379367152250879
heriyanto <heriyanto@Changes-MacBook-Pro.local>
2017-03-15 15:46:14 +0700

    attach new volume

--- instance.tf
+++ instance.tf
@@ -26,6 +26,21 @@ resource "aws_instance" "example" {
   }
 }
 
+resource "aws_ebs_volume" "ebs-volume-1" {
+    availability_zone = "ap-southeast-1a"
+    size = 20
+    type = "gp2"
+    tags {
+        Name = "extra volume data"
+    }
+}
+
+resource "aws_volume_attachment" "ebs-volume-1-attachment" {
+  device_name = "/dev/xvdh"
+  volume_id = "${aws_ebs_volume.ebs-volume-1.id}"
+  instance_id = "${aws_instance.example.id}"
+}
+
 output "ip" {
     value = "${aws_instance.example.public_ip}"
 }
